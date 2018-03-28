be3b4de3d886410ba62e2560bfb80abf6b7ee8d3
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-03-27 20:42:06 -0700

    Update Ch9.sh

--- Ch9.sh
+++ Ch9.sh
@@ -26,3 +26,14 @@ echo
 sudo iptables -L 
 sleep 2
 
+echo "Show TCP connection"
+echo 
+sudo netstat 
+sleep 2
+
+echo "Enable IP forwarding by setting value 3"
+echo 
+sysct1 -w net.ipv4.ip_forward=3
+sleep 
+
+clear 
